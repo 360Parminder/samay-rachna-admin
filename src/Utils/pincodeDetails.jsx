39ec01110626fc2988dd383d615997c88d75ba4b
360Parminder <yesask8@gmail.com>
2025-01-27 18:27:16 +0530

    fix: update pincodeDetails to use Vite environment variables for API URL and key

--- src/Utils/pincodeDetails.jsx
+++ src/Utils/pincodeDetails.jsx
@@ -1,18 +1,12 @@
 import axios from "axios";
 
 export const pincodeDetails = async (pincode) => {
-  // console.log(process.env.REACT_APP_PINCODE_API_URL, process.env.REACT_APP_PINCODE_API_KEY);
-  
-  // if (!process.env.REACT_APP_PINCODE_API_URL || !process.env.REACT_APP_PINCODE_API_KEY) {
-  //   console.error("Missing API configuration in environment variables.");
-  //   throw new Error("API configuration is not set.");
-  // }
 
   const options = {
     method: 'GET',
-    url: `https://india-pincode-with-latitude-and-longitude.p.rapidapi.com/api/v1/pincode/${pincode}`,
+    url: `${import.meta.env.VITE_PINCODE_API_URL}${pincode}`,
     headers: {
-      'x-rapidapi-key': "b9a9a1c3c9msh2aede9a85e4a17fp111364jsnf6df7a3b6a5d",
+      'x-rapidapi-key': import.meta.env.VITE_PINCODE_API_KEY,
       'x-rapidapi-host': 'india-pincode-with-latitude-and-longitude.p.rapidapi.com',
     },
   };
